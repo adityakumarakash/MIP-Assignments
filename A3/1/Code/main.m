0051daae11d52477cd3d12c935e62b6b44e57e72
Aditya Kumar Akash <adityakumarakash@gmail.com>
2016-03-20 11:58:03 +0530

    Added transformation updates

--- A3/1/Code/main.m
+++ A3/1/Code/main.m
@@ -4,6 +4,7 @@
 % Loading the images
 load('../Data/assignmentImageReconstructionPhantom.mat');
 imageNoisy = ifft2(imageKspaceData);
+Y = imageKspaceData;
 S = imageKspaceMask;
 
 % parameters
@@ -11,11 +12,11 @@ alpha = 0.08;
 gamma = 0.01;
 epsilonThreshold = 0.01;
 epsilon = 0.000001;
-gradThreshold = 0.0001;
+gradThreshold = 0.00001;
 
-% minVal = Inf;
-% minAlpha = 0;
-% minGamma = 0;
+minVal = Inf;
+minAlpha = 0;
+minGamma = 0;
 
 fxValues = zeros(3, 100, 1);
 iterValues = zeros(3, 1);
@@ -23,8 +24,8 @@ denoisedImages = zeros(3, size(imageNoisy, 1), size(imageNoisy, 2));
 mult = [1,1; 1.2,1; 0.8, 1; 1,1.2; 1,0.8];
 rrmseValues = zeros(3, 5, 1);
 
-for prior = 1:3
-   
+for prior = 3:3
+   prior
    % selecting the optimal parameters
    if prior == 1
        alpha = 0.20;
@@ -37,8 +38,8 @@ for prior = 1:3
        gamma = 0.04;
    end
    
-%   for alpha = linspace(0.58,0.63,6)
-%       for gamma = linspace(0.14, 0.19, 6)
+  for alpha = linspace(0.01,0.1,10)
+      for gamma = linspace(0.01, 0.1, 10)
 %           prior = 2;
 
     % find the derivative w.r.t prior and the penalty w.r.t potential
@@ -57,11 +58,11 @@ for prior = 1:3
         h = @(x) discontAdapFunctionH(x, gamma);
         g = @(x) discontAdapFunctionG(x, gamma);
     end
-    
-    for index = 1:5
 
-            alpha = alpha * mult(index, 1);
-            gamma = gamma * mult(index, 2);
+    %for index = 1:5
+
+%             alpha = alpha * mult(index, 1);
+%             gamma = gamma * mult(index, 2);
 
             % Gradient Descent to optimize the values
             X = imageNoisy;           % initial guess
@@ -70,34 +71,35 @@ for prior = 1:3
             tau = 0.1;
             prevFx = alpha * priorPotential(X, imageNoisy, g);
             iteration = 1;  
-            if index == 1
-                fxValues(prior, iteration) = prevFx;
-            end
+%             if index == 1
+%                 fxValues(prior, iteration) = prevFx;
+%             end
             
             while tau > epsilon && iteration < 50
                 % main loop 
 
                 % calculating the likelihood part of X
-                likelihoodX = prevX - imageNoisy;
+                likelihoodX = Y - S.*fft2(prevX);
 
-                priorDerivative = priorGradient(X, imageNoisy, h);
-
-                dX = (1-alpha) * 2 * likelihoodX + alpha * priorDerivative;
+                priorDerivative = priorGradient(prevX, imageNoisy, h);
+                likelihoodDerivative = ifft2(S.*fft2(prevX)) - imageNoisy;
+                dX = (1-alpha) * 2 * likelihoodDerivative + alpha * priorDerivative;
 
                 if sum(sum(abs(dX))) < gradThreshold
                     break
                 end
 
                 X = prevX - tau * dX;
+                
                 priorV = priorPotential(X, imageNoisy, g);
-                likelihoodX = X - imageNoisy;
+                likelihoodX = Y - S.*fft2(X);
                 fx = alpha * priorV + (1-alpha) * sum(sum(abs(likelihoodX).^2));
 
                 % selecting required value of tau
                 while  fx > prevFx && tau > epsilon
                     X = prevX - tau * dX;
                     priorV = priorPotential(X, imageNoisy, g);
-                    likelihoodX = X - imageNoisy;
+                    likelihoodX = Y - S.*fft2(X);
                     fx = alpha * priorV + (1-alpha) * sum(sum(abs(likelihoodX).^2));
                     tau = tau * 0.5;
                 end
@@ -106,93 +108,94 @@ for prior = 1:3
                     prevX = X;
                     prevFx = fx;
                     iteration = iteration + 1;
-                    if index == 1
-                        fxValues(prior, iteration) = fx;
-                    end
+%                     if index == 1
+%                         fxValues(prior, iteration) = fx;
+%                     end
                     tau = tau * 1.1;
                 end 
 
             end
 
             X = prevX;
-            if index == 1
-                iterValues(prior) = iteration;
-                denoisedImages(prior,:,:) = X;
-            end
-            rrmseValues(prior, index) = RRMSE(imageNoiseless, X);
+%             if index == 1
+%                 iterValues(prior) = iteration;
+%                 denoisedImages(prior,:,:) = X;
+%             end
+%             rrmseValues(prior, index) = RRMSE(imageNoiseless, X);
 
-    %         sprintf('%f %f %f\n', RRMSE(imageNoiseless, X), alpha, gamma)
-    %         if RRMSE(imageNoiseless, X) < minVal
-    %             minVal = RRMSE(imageNoiseless, X);
-    %             minAlpha = alpha;
-    %             minGamma = gamma;
-    %         end
+            sprintf('%f %f %f %d\n', RRMSE(imageNoiseless, X), alpha, gamma, iteration)
+            if RRMSE(imageNoiseless, X) < minVal
+                minVal = RRMSE(imageNoiseless, X);
+                minAlpha = alpha;
+                minGamma = gamma;
+            end
 
         end
+  end
 end
 
     % 
-    % minVal
-    % minAlpha
-    % minGamma
+    minVal
+    minAlpha
+    minGamma
 
 
     % gradient descent done
     % show the optimized values
-    % disp(RRMSE(imageNoiseless, X));
-    % imshow(imageNoiseless);
-    % figure; imshow(imageNoisy);
-    % figure; imshow(X);
+    disp(RRMSE(imageNoiseless, X));
+    imshow(imageNoiseless);
+    figure; imshow(imageNoisy);
+    figure; imshow(X);
 
 
-    % part A
-    % The initial RRMSE
-    disp(RRMSE(imageNoiseless, imageNoisy));
+%     % part A
+%     % The initial RRMSE
+     disp(RRMSE(imageNoiseless, imageNoisy));
 
-%% part B
-
-for prior = 1:3
-    if prior == 1
-        fprintf('For quadratic prior, alpha* = 0.2\n');
-    elseif prior == 2
-        fprintf('For Discontinuity-adaptive Huber function, alpha* = 0.60, gamma*= 0.15\n');
-    else
-        fprintf('For Discontinuity-adaptive function, alpha* = 0.83, gamma*= 0.04\n');
-    end
-
-    if prior ~= 1
-        for index = 1:5
-            fprintf('RRMSE at %.1falpha*, %.1fgamma* = %f\n', mult(index, 1), mult(index, 2), rrmseValues(prior, index));
-        end
-    else
-        for index = 1:3
-            fprintf('RRMSE at %.1falpha*= %f\n', mult(index, 1), rrmseValues(prior, index));
-        end
-    end
-end
-
-%% part C
-
-figure; imshow(imageNoiseless); title ('NoiseLess Image');
-figure; imshow(abs(imageNoisy));     
-X(:,:) = denoisedImages(1,:,:); title ('Noisy Image');
-figure; imshow(abs(X));
-X(:,:) = denoisedImages(2,:,:);title ('Quadratic Function Image');
-figure; imshow(abs(X));
-X(:,:) = denoisedImages(3,:,:);  title ('Huber function Image');
-figure; imshow(abs(X));title ('Discontinuous Adaptive Image');
-
-%% part D
-for prior = 1:3
-    figure;
-    plot(fxValues(prior, 1:iterValues(prior)));
-    if prior == 1
-        title('Quadratic Prior');
-    elseif prior == 2
-        title('Discontinuous Adaptive Huber Prior');
-    else 
-        title('Discontinuous Adaptive Prior');
-    end
-    xlabel('Iteration number');
-    ylabel('Objective function');
-end
+% %% part B
+% 
+% for prior = 1:3
+%     if prior == 1
+%         fprintf('For quadratic prior, alpha* = 0.2\n');
+%     elseif prior == 2
+%         fprintf('For Discontinuity-adaptive Huber function, alpha* = 0.60, gamma*= 0.15\n');
+%     else
+%         fprintf('For Discontinuity-adaptive function, alpha* = 0.83, gamma*= 0.04\n');
+%     end
+% 
+%     if prior ~= 1
+%         for index = 1:5
+%             fprintf('RRMSE at %.1falpha*, %.1fgamma* = %f\n', mult(index, 1), mult(index, 2), rrmseValues(prior, index));
+%         end
+%     else
+%         for index = 1:3
+%             fprintf('RRMSE at %.1falpha*= %f\n', mult(index, 1), rrmseValues(prior, index));
+%         end
+%     end
+% end
+% 
+% %% part C
+% 
+% figure; imshow(imageNoiseless); title ('NoiseLess Image');
+% figure; imshow(abs(imageNoisy));     
+% X(:,:) = denoisedImages(1,:,:); title ('Noisy Image');
+% figure; imshow(abs(X));
+% X(:,:) = denoisedImages(2,:,:);title ('Quadratic Function Image');
+% figure; imshow(abs(X));
+% X(:,:) = denoisedImages(3,:,:);  title ('Huber function Image');
+% figure; imshow(abs(X));title ('Discontinuous Adaptive Image');
+% 
+% %% part D
+% for prior = 1:3
+%     figure;
+%     plot(fxValues(prior, 1:iterValues(prior)));
+%     if prior == 1
+%         title('Quadratic Prior');
+%     elseif prior == 2
+%         title('Discontinuous Adaptive Huber Prior');
+%     else 
+%         title('Discontinuous Adaptive Prior');
+%     end
+%     xlabel('Iteration number');
+%     ylabel('Objective function');
+% end
