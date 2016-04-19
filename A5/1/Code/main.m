87dc3bf4988a236c2b340233a3590fc18ceaed17
adityakumarakash <adityakumarakash@gmail.com>
2016-04-20 00:13:27 +0530

    5 done

--- A5/1/Code/main.m
+++ A5/1/Code/main.m
@@ -1,3 +1,9 @@
+%% CS 736: Medical Image Processing
+% Question 1
+% Assignment 5
+% Praveen Agrawal 12D020030
+% Aditya Kumar 120050046
+
 %% Loading Data
 load('../Data/assignmentShapeAnalysis.mat');
 
@@ -15,18 +21,25 @@ oldMean = mean + 100;
 
 % parameters for loop
 epsilon = 0.000001;
-maxIteration = 50;
 iteration = 0;
 
 % loop
-while sqrt(sumsqr(mean - oldMean)) > epsilon && iteration < maxIteration
+while sqrt(sumsqr(mean - oldMean)) > epsilon 
     % update in the loop
     oldMean = mean;
     iteration = iteration + 1;
     
     % for each set find the rotation matrix
     for i = 1 : numOfPointSets
-        RotMatrix = findRotationMatrix(mean, preShapePoints(:, :, i));
+        % find rotation
+        Y = preShapePoints(:, :, i);
+        [U, ~, V] = svd(Y * mean');
+        RotMatrix = V * U';
+        if det(RotMatrix) == -1
+            % correcting for negation
+            t = [1, 0; 0, -1];
+            RotMatrix = V * t * U';
+        end
         preShapePoints(:, :, i) = RotMatrix * preShapePoints(:, :, i);
     end
     
@@ -38,104 +51,77 @@ while sqrt(sumsqr(mean - oldMean)) > epsilon && iteration < maxIteration
     mean = mean ./ norm;
 end
 
+% calculation of eigen values done here
+CV = 0;
+for i = 1 : numOfPointSets
+    CV = CV + (preShapePoints(:,:,i) - mean) * (preShapePoints(:,:,i) - mean)';
+end
+% normalized
+CV = CV / numOfPointSets;
+[V,D] = eig(CV);
+eigenValues = diag(D);
+
+%% calculation of modes of variation
+var = zeros(size(mean, 1), size(mean, 2), 2, 2);
+for j = 1 : 2
+    for k = 1 : 2
+        for i = 1 : 2
+            sd = sqrt(eigenValues(3 - j));
+            mult = 1;
+            if k == 2
+                mult = -1;
+            end
+            var(i, :, k, j) = mean(i, :) + mult * 2 * sd * V(:, 3 - j)' * mean; 
+        end
+    end
+end
+
+%% PLOTS
+% using color shceme
 colors = jet(numOfPointSets);
 
-
-%% Intial point set
+%% Part(a) Intial point set
 figure;
 hold on;
-title('Original Point Sets');
+title('Original Point Sets plot');
 for i= 1 : numOfPointSets
-   scatter(pointSets(1,:,i), pointSets(2,:,i), 8, 'MarkerFaceColor', colors(i, :)); 
+   scatter(pointSets(1,:,i), pointSets(2,:,i), 'MarkerFaceColor', colors(i, :)); 
 end
 hold off;
 
 
-
-%% Aligned point sets
+%% Part(b) Aligned point sets with mean
 figure;
 hold on;
-plot(mean(1,:),mean(2,:),'-x','LineWidth',4,'Marker','*');
+title('Aligned Point Sets with mean is plotted');
 for i=1:numOfPointSets
-   scatter(preShapePoints(1,:,i),preShapePoints(2,:,i),...
-       4,'MarkerFaceColor',colors(i,:)); 
+   scatter(preShapePoints(1, :, i),preShapePoints(2, :, i), 'MarkerFaceColor',colors(i, :)); 
 end
-title('Aligned Point Sets');
-hold off
-
-xl = xlim;
-yl = ylim;
-
-figure;
-scatter(mean(1,:),mean(2,:));
-xlim(xl);
-ylim(yl);
-title('Final Mean shape');
-
-%% Computing principal modes of variation
-
-cov = 0;
-% mean subtracted vectorized points
-for i=1:numOfPointSets
-    pointSet = preShapePoints(:,:,i) - mean;
-    cov = cov + pointSet*pointSet';
-end
-cov = cov/numOfPointSets;
-
-
-% Eigenvalue decomposition
-[V,D] = eig(cov);
-
-eigenvals = diag(D);
-
-figure;
-scatter(1:length(eigenvals),eigenvals(end:-1:1));
-title('Variances along each principal mode');
-
-
-%% Calculating shapes corresponding to modes of variations
-
-% Corresponding to 2*sd along 1st eigenvector
-sd = sqrt(eigenvals(2));
-ps1_1(1,:) = mean(1,:) + 2*sd*V(:,2)'*mean;
-ps1_1(2,:) = mean(2,:) + 2*sd*V(:,2)'*mean;
-
-
-% Corresponding to -2*sd along 1st eigenvector
-sd = sqrt(eigenvals(2));
-ps2_1(1,:) = mean(1,:) - 2*sd*V(:,2)'*mean;
-ps2_1(2,:) = mean(2,:) - 2*sd*V(:,2)'*mean;
-
-% Corresponding to 2*sd along 2nd eigenvector
-sd = sqrt(eigenvals(1));
-ps1_2(1,:) = mean(1,:) + 2*sd*V(:,1)'*mean;
-ps1_2(2,:) = mean(2,:) + 2*sd*V(:,1)'*mean;
-
-
-
-% Corresponding to -2*sd along 2nd eigenvector
-sd = sqrt(eigenvals(1));
-ps2_2(1,:) = mean(1,:) - 2*sd*V(:,1)'*mean;
-ps2_2(2,:) = mean(2,:) - 2*sd*V(:,1)'*mean;
-
-%% Plotting variations
-figure;
-hold on;
-scatter(ps1_1(1,:),ps1_1(2,:),'ro');
-scatter(ps2_1(1,:),ps2_1(2,:),'mo');
-scatter(mean(1,:),mean(2,:),'b*');
-xlim(xl);
-ylim(yl);
-legend('+2sd','-2sd','mean');
-title('1st mode of variation');
+plot(mean(1,:),mean(2,:), 'Marker', '*');
 hold off;
 
+
+%% Part(c) Principal modes
 figure;
+scatter(1 : size(eigenValues, 1), eigenValues, '*');
+title('variances along principal modes in ascending');
+
+
+%% Part(d) Plot modes of variations
+% plot first mode of variation
+figure;
+title('First Mode Variation');
 hold on;
-scatter(ps1_2(1,:),ps1_2(2,:),'ro');
-scatter(ps2_2(1,:),ps2_2(2,:),'mo');
-scatter(mean(1,:),mean(2,:),'b*');
-xlim(xl);
-ylim(yl);
-legend('+2sd','-2sd','mean');
+scatter(mean(1, :),mean(2, :),'*');
+scatter(var(1, :, 1, 1), var(2, :, 1, 1),'o');
+scatter(var(1, :, 2, 1), var(2, :, 2, 1),'+');
+hold off;
+
+% plot second mode of variation
+figure;
+title('Second Mode Variation');
+hold on;
+scatter(mean(1, :),mean(2, :),'*');
+scatter(var(1, :, 1, 2), var(2, :, 1, 2),'o');
+scatter(var(1, :, 2, 2), var(2, :, 2, 2),'+');
 hold off;
